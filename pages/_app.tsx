925f47ae6fb5ad468b1a7a794d787da92c3708b9
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-09-19 21:54:36 -0500

    end next basic course

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import { AppProps } from 'next/app';
+import { AppProps, NextWebVitalsMetric } from 'next/app';
 import '@assets/css/normalize.css';
 import '../styles.css';
 import Layout from '@components/Layout';
+
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   // Providers - Context/Providers, Theme, data
